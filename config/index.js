b3bd10f780b4e9bdac802e13c9cb368d97f2b861
xiaowu <943915349@qq.com>
2023-05-27 17:43:30 +0800

    update

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
     // 代理列表, 是否开启代理通过[./dev.env.js]配置
     proxyTable: devEnv.OPEN_PROXY === false ? {} : {
       '/proxyApi': {
-        target: 'http://localhost:88/renren-fast/',
+        target: 'http://localhost:88/api',
         changeOrigin: true,
         pathRewrite: {
           '^/proxyApi': '/'
